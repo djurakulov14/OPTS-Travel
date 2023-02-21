e169f20ea6f9c55f68d7f8d9f27fb115f0e82176
djurakulov14 <109537648+djurakulov14@users.noreply.github.com>
2023-02-21 22:30:17 +0500

    added hotels and 1 city

--- src/pages/api/data/hotels.js
+++ src/pages/api/data/hotels.js
@@ -4,104 +4,183 @@ export const hotels = [
         cityId:2,
         title: "Asian Plaza",
         stars: 4,
-        link:"https://www.booking.com/Share-l2RMXO"
+        link:"https://www.booking.com/Share-l2RMXO",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/403339301.jpg?k=9bde94acb6335a383d692790ca64818eeb966fe5fd4373b9a75ee984048a2f6e&o=&hp=1"
     },
     {
         id: 2,
         cityId:2,
         title: "Paradise Plaza Luxury",
         stars: 4,
-        link:"https://www.booking.com/Share-Fex5ZA"
+        link:"https://www.booking.com/Share-Fex5ZA",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/421102436.jpg?k=910173f0a55dddcc8034c83e401654d48e752d7176fa5823f0dc38b72eec28d4&o=&hp=1"
     },
     {
         id: 3,
         cityId:2,
         title: "Boutique Hotel Minzifa",
         stars: 4,
-        link:"https://www.booking.com/Share-FjoqSrX"
+        link:"https://www.booking.com/Share-FjoqSrX",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/10670790.jpg?k=710c153d0a3223d53fb43171f109721f836ab3f715bd4824fc1027bcc2f7d7c8&o=&hp=1"
     },
     {
         id: 4,
         cityId:2,
         title: "Hotel Volida Boutique",
         stars: 4,
-        link:"https://www.booking.com/Share-oFXFyS"
+        link:"https://www.booking.com/Share-oFXFyS",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/139237147.jpg?k=22ba37152fa108c3aff04a966fe8dd4f6764ec5f3aba8ff72c12db0f6d7228a2&o=&hp=1"
     },
     {
         id: 5,
         cityId:2,
         title: "Rayyan Hotel Bukhara",
         stars: 4,
-        link:"https://www.booking.com/Share-se2DCT"
+        link:"https://www.booking.com/Share-se2DCT",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/331202478.jpg?k=4ccf88a8e460083df0f689ecebbe3004eb471ef0e95aa79fe85eaeba66c58706&o=&hp=1"
     },
     {
         id: 6,
         cityId:2,
         title: "Orient Star Varaxsha",
         stars: 4,
-        link:"https://www.booking.com/Share-3ZUzUK"
+        link:"https://www.booking.com/Share-3ZUzUK",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/339366813.jpg?k=9d91fff97bff8759907d4feb7099685dd9f10c8b0d896c29fe31aa5d64580fde&o=&hp=1"
     },
     {
         id: 7,
         cityId:2,
         title: "CARAVAN Plaza",
         stars: 4,
-        link:"https://www.booking.com/Share-se9GaZ"
+        link:"https://www.booking.com/Share-se9GaZ",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/344763178.jpg?k=c09270821802c94ebc01ff7f96e3067873898e0fead168194641d362ced5d5e3&o=&hp=1"
     },
     {
         id: 8,
         cityId:2,
         title: "Mukhlisabegim Hotel",
         stars: 4,
-        link:"https://www.booking.com/Share-Ekow5s7"
+        link:"https://www.booking.com/Share-Ekow5s7",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/274071994.jpg?k=9784f919c82e7a1ef7c37ac80a7cc3a7e642ea2adc326cd299ccce278dcfb2f3&o=&hp=1"
     },
     {
         id: 9,
         cityId:2,
         title: "Hotel GOLDEN MINARET",
         stars: 4,
-        link:"https://www.booking.com/Share-THKQwm"
+        link:"https://www.booking.com/Share-THKQwm",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/383533075.jpg?k=1465427a2532f8f8f9ddc475a332b0e6b42dd4d601abb7b4923d248f4bc64d94&o=&hp=1"
     },
     {
         id: 10,
         cityId:2,
         title: "Labi Rud Hotel Premium",
         stars: 4,
-        link:"https://www.booking.com/Share-lH9FHK"
+        link:"https://www.booking.com/Share-lH9FHK",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/401049529.jpg?k=a998a7e6a160396ed99362d67ba4599e761d9a266d78fa9c4fc406d6718ab1ec&o=&hp=1"
     },
     {
         id: 11,
         cityId:2,
         title: "Hotel Malika Bukhara",
         stars: 4,
-        link:"https://www.booking.com/Share-8xxr3o"
+        link:"https://www.booking.com/Share-8xxr3o",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/300052191.jpg?k=8a7fafaea49304758f11357312ad173c252b6c107b959f9ba6447ec080120999&o=&hp=1"
     },
     {
         id: 12,
         cityId:2,
         title: "Boutique Safiya",
         stars: 4,
-        link:"https://www.booking.com/Share-tHhGe3"
+        link:"https://www.booking.com/Share-tHhGe3",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/141816696.jpg?k=c6736b9c24d78145bd16030802bb7ac018e5a100445941e8e11b13de4b40a57c&o=&hp=1"
     },
     {
         id: 13,
         cityId:2,
         title: "Anor",
         stars: 4,
-        link:"https://www.booking.com/Share-Lpb00k"
+        link:"https://www.booking.com/Share-Lpb00k",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/362972480.jpg?k=4b96b57314e54c96fc403c9150b44219b53980df9c74f79b1086c83e8fa0dd4c&o=&hp=1"
     },
     {
         id: 14,
         cityId:2,
         title: "Sahid Zarafshon",
         stars: 4,
-        link:"https://www.booking.com/Share-GM7pTX"
+        link:"https://www.booking.com/Share-GM7pTX",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/269095427.jpg?k=4c1a09beccf27beb5b76f9909174e015b1ef9b13f6e0b93daa054991f048e379&o=&hp=1"
     },
     {
         id: 15,
         cityId:2,
         title: "Sultan Hotel Bukhara",
         stars: 4,
-        link:"https://www.booking.com/Share-p1pKq9"
+        link:"https://www.booking.com/Share-p1pKq9",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/302787568.jpg?k=477dd543ce3ad36fe0ff66cc27212e8b2ae20c41cfd356accb77e135fc5f6e89&o=&hp=1"
+    },
+    {
+        id: 16,
+        cityId:2,
+        title: "Turon City Plaza",
+        stars: 4,
+        link:"https://www.booking.com/Share-XiDeGd",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/382637175.jpg?k=6beadaea0c2013fb5f707f38d92ac30dcc6daf8610464885cbf6bb1a04214624&o=&hp=1"
+    },
+    {
+        id: 17,
+        cityId:2,
+        title: "Omar Khayyam Hotel",
+        stars: 4,
+        link:"https://www.booking.com/Share-95NVvk",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/69752310.jpg?k=64bdb21919a9cb05dcb312e43bc1fa0eac55f8c1510ccc2df5b7942d5512bf95&o=&hp=1"
+    },
+    {
+        id: 18,
+        cityId:2,
+        title: "Garden Plaza Bukhara",
+        stars: 4,
+        link:"https://www.booking.com/Share-3FJGsi",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/331984410.jpg?k=5255c500976e5ee7b5d2fb02e42e5d8aeee5b90afd4d5021d4da66ced89298e4&o=&hp=1"
+    },
+    {
+        id: 19,
+        cityId:2,
+        title: "Khujra Studios",
+        stars: 4,
+        link:"https://www.booking.com/Share-S0z2FC",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/319797249.jpg?k=caac1ea4454cfe4fbab0249e2c1d52bbce8af761266404a1ffcf4ae936bd8524&o=&hp=1"
+    },
+    {
+        id: 20,
+        cityId:2,
+        title: "DUNYO PALACE",
+        stars: 4,
+        link:"https://www.booking.com/Share-2dFnNA",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/357040272.jpg?k=7505c489ee3facf04747796b0179f10fbaf7872034ff9225385dcb242ee8359a&o=&hp=1"
+    },
+    {
+        id: 21,
+        cityId:2,
+        title: "Royal Bukhara hotel",
+        stars: 4,
+        link:"https://www.booking.com/Share-CQl0rJy",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/236957216.jpg?k=3552d2543a6e7512d6eff11504bfb3667e0ae82f733a8811ee54af3b485925bf&o=&hp=1"
+    },
+    {
+        id: 22,
+        cityId:2,
+        title: "Poykent Bukhara Naqshband",
+        stars: 4,
+        link:"https://www.booking.com/Share-sxJQXH",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/424379904.jpg?k=be08880d3320c94184ae5c108079c95b4ede1c9970554a4fd5e05fde1bf43d70&o=&hp=1"
+    },
+    {
+        id: 23,
+        cityId:2,
+        title: "Hotel Ulug'bek",
+        stars: 4,
+        link:"https://www.booking.com/Share-WHVN1V",
+        img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/398786123.jpg?k=ddb71e2541cc0f2b5d20febe3bdea7576dcc13621ed9d441d71029d621bd141f&o=&hp=1"
     }
 ]
