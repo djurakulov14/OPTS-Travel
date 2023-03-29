bb6878fc5ac59ed3deea975d211a344e0c7013c5
djurakulov14 <109537648+djurakulov14@users.noreply.github.com>
2023-03-29 13:27:01 +0500

    fixed bug

--- src/pages/cityPage/index.jsx
+++ src/pages/cityPage/index.jsx
@@ -9,7 +9,7 @@ import { useTranslation } from 'next-i18next'
 import Head from 'next/head'
 
 export async function getStaticProps({locale}) {
-  const res = await fetch(locale === "ru" ? "https://opts-travel.netlify.app/api/city" : "https://opts-travel.netlify.app/api/cityEn")
+  const res = await fetch("https://opts-travel.netlify.app/api/city")
   const data = await res.json()
 
 
